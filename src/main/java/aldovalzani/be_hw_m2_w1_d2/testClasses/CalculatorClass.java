2f4e230eea7c4b7efa027b51df846fa0aae23cf3
xAldox995 <valzanialdo@gmail.com>
2024-10-23 15:34:19 +0200

    primo test inserito

---
+++ src/main/java/aldovalzani/be_hw_m2_w1_d2/testClasses/CalculatorClass.java
@@ -0,0 +1,7 @@
+package aldovalzani.be_hw_m2_w1_d2.testClasses;
+
+public class CalculatorClass {
+    public int sum(int n1, int n2) {
+        return n1 + n2;
+    }
+}
--- src/test/java/aldovalzani/be_hw_m2_w1_d2/BeHwM2W1D2ApplicationTests.java
+++ src/test/java/aldovalzani/be_hw_m2_w1_d2/BeHwM2W1D2ApplicationTests.java
@@ -1,13 +1,18 @@
 package aldovalzani.be_hw_m2_w1_d2;
 
+import aldovalzani.be_hw_m2_w1_d2.testClasses.CalculatorClass;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BeHwM2W1D2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testSomma() {
+        CalculatorClass calcolatrice = new CalculatorClass();
+        int result = calcolatrice.sum(1, 6);
+        Assertions.assertEquals(7, result);
+    }
 
 }
